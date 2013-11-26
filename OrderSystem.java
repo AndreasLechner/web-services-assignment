beee3e826f556362a185ad7dac19840f2e45c551
Roman Yakobnyuk <y@kobnyuk.pro>
2013-11-26 10:15:16 +1000

    Working version with randomly generated prices

--- OrderSystem.java
+++ OrderSystem.java
@@ -1,62 +1,139 @@
 import java.util.*;
 /**
- * Write a description of class PaymentSystem here.
+ *
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class OrderSystem {
-    public static void main (String []args) {   
 
+
+    private static List<Part> parts;
+    private static List<Order> orders;
+
+
+    public static void main (String []args) {
+
+        Random random = new Random();
+
+        parts = new ArrayList<Part>();
+        orders = new ArrayList<Order>();
+
+        for (int i = 0; i < 10000; i++) {
+            String code;
+
+            if (i < 10) {
+                code = "000" + i;
+            } else {
+                if (i < 100) {
+                    code = "00" + i;
+                } else {
+                    if (i < 1000) {
+                        code = "0" + i;
+                    } else {
+                        code = "" + i;
+                    }
+                }
+            }
+
+            parts.add(new Part("PN" + code, random.nextInt(1000), (random.nextInt(101) > 20 ) ));
+        }
+
+        placeOrder(4, "PN2221", "asdasfdasdw");
     }
     
         /**
      * 
      */
-    public static String makeOrder (int customerID, String partCode, String email) {
-        String output  = "";
-        boolean check = false;
-        boolean length = false;
+    public static void placeOrder (int customerID, String partCode, String email) {
+        
+        boolean error = false;
+        String errorMessage;
 
-        if (customerID > 1 && customerID < 10) {
-            if (partCode.length() != 6) {       
-                for (int i = 2; i < 6; i++) {
-                    if (partCode.charAt(i) >= 0 && partCode.charAt(i) < 10) {
-                        length = true;
-                    }
-                    else 
-                        length = false;                    
-                }
-                if (length = true) {
-                    output = "Order Success. Reciept order for Customer ID: " + customerID;
-                    check = true;
-                }
-                else 
-                    output = "Invalid part ID";
-            }
-            else
-                output = "Invalid part ID";
+        if ( customerID < 2 && customerID > 9 ) {
+            error = true; 
+            errorMessage = " Customer ID is out of range (1 < customerID < 10) ";
+            answer(errorMessage, "error", email);
         }
-        else
-            output = "Invalid Customer ID.";
 
-        return generateXML(output,check,email);
+        if ( partCode.length() != 6 ) {
+            error = true;
+            errorMessage = " Part code is wrong. Requiered format is PNXXXX ";
+            answer(errorMessage, "error", email);
+        } else {
+            if ( !partCode.startsWith("PN") ) {
+                error = true;
+                errorMessage = " Part code should start with PN followed by 4 digits";
+                answer(errorMessage, "error", email);
+            } else {
+                try {
+                    int numberCode = Integer.parseInt(partCode.substring(2));
+                } catch (Exception e) {
+                    error = true;
+                    answer(" Number part of part code is not valid ", "error", email);
+                }
+
+            }
+        }
+
+        if ( !error ) {
+            orders.add( new Order(email, partCode) );
+            answer(" Your order for " + partCode + " was Successful!", "success", email );
+        }
     }
 
     /**
      * 
      */
-    private static String generateXML (String output, Boolean check, String email) {
-        String xml = "<rootnode>\n<header>";
+    private static void answer (String message, String type, String email) {
 
-        if (check)
-            xml = xml + "<Title>Order Successful</Title>\n<Email>" +email +  "</Email>\n</header>\n";
-        else
-            xml = xml + "<Title>Error with Details</Title>\n<Email>" +email +  "</Email>\n</header>\n";
+        String answer = "<rootnode>\n<header>" ;
 
-        xml = xml +"<body>" + output + "</body>\n </rootnode>";
+        if ( type.equals("error") ) {
+            answer = answer + "<Title>Error occured: " + message + " . Order was not completed.</Title>\n<Email>" +email +  "</Email>\n</header>\n";
+        } else {
+            answer = answer + "<Title>Order Successful</Title>\n<Email>" +email +  "</Email>\n</header>\n";
+        }
+        
+        answer = answer +"<body>" + message + "</body>\n </rootnode>";
 
-        return xml;
+        System.out.print(answer);
 
+    }
+
+    public static class Part {
+        private int price;
+        private boolean available;
+        private String partCode;
+        
+        public Part(String pC, int p, boolean aval) {
+            price = p;
+            available = aval;
+            partCode = pC;
+        }
+        
+
+        public boolean isAvailable() {
+            return available;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public String getPartCode() {
+            return partCode;
+        }
+
+    }
+
+    public static class Order {
+        private static String email;
+        private static String partCode;
+
+        public Order(String em, String pC) {
+            email = em;
+            partCode = pC;
+        }
     }
 }
